b126135b9c69b59cc111b8c811ff2c80333c1304
kuznetsovkr <kuznetsovkr@yandex.ru>
2023-06-21 15:21:17 +0700

    мидлвейр auth

--- middlewaree/authMiddleware.js
+++ middlewaree/authMiddleware.js
@@ -9,13 +9,12 @@ module.exports = function (req, res, next) {
     try {
         const token = req.headers.authorization.split(' ')[1]
         if (!token) {
-            return res.status(403).json({message: "Пользователь не авторизован"})
+            return res.status(401).json({message: "Auth error"})
         }
         const decodedData = jwt.verify(token, secret)
         req.user = decodedData
         next()
     } catch (e) {
-        console.log(e)
-        return res.status(403).json({message: "Пользователь не авторизован"})
+        return res.status(401).json({message: "Пользователь не авторизован"})
     }
 };
